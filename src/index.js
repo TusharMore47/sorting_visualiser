5bf124d47ae80a71bde5132d5eb313b6530f1318
Tushar More <tsmore0905@gmail.com>
2022-08-04 14:47:19 +0530

    add visualizer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import SortingVisualizer from "./SortingVisualizer/SortingVisualizer.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ## Rendering SortingVisualizer Component ( Imported from path mentioned above ) ## //
+ReactDOM.render(<SortingVisualizer />, document.getElementById("root"));
